5f2a3649630bd7b519affdb0d9248e01cbc94982
Jorge <jorgeradasilva@gmail.com>
2021-04-18 01:56:29 +0100

    added request headers

--- Controlers/Restaurant.controler.js
+++ Controlers/Restaurant.controler.js
@@ -3,6 +3,18 @@ const db = require('../Database/Database');
 const app = express();
 
 
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+ 
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
 
 app.get('/restaurantes',async (req, res)=>{
     db.Restaurante.findAll({ include:
