0f1d5c635f04c2022b1a658fce8f7bbf4ec871ca
Jorge <jorgeradasilva@gmail.com>
2021-04-25 19:56:05 +0100

    set log to false

--- Database/DatabaseCon.js
+++ Database/DatabaseCon.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const con = new Sequelize('pedeagoratest', 'pedeagorauser' , 'bM3rJv5vKOg9xmu5',{
         host:'localhost',
         dialect:'mariadb',
+        logging:false
     },
     { 
         define: {
