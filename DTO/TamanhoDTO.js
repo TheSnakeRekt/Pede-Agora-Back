71b2c24d237109980106f5b9b982c81598cb1e17
Jorge <jorgeradasilva@gmail.com>
2021-05-09 20:32:36 +0100

    More changes to DTO in back

--- DTO/TamanhoDTO.js
+++ DTO/TamanhoDTO.js
@@ -2,13 +2,13 @@ const GrupoDTO = require("./GrupoDTO")
 
 module.exports = class TamanhoDTO {
     static mapper(data){
-        console.log(data);
+
         return{
             id:data.get('id'),
             preco:data.get('preco'),
             nome:data.get('nome'),
             default:data.get('default'),
-            grupos:data.get('grupos') ? data.get('grupos').map(GrupoDTO.mapper) : []
+            grupos:data.get('OpcaoGrupoTamanho') ? data.get('OpcaoGrupoTamanho').map(GrupoDTO.mapper) : []
         }
     }
 }
